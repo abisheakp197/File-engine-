2621e7776d1298c983a2ef4084a42546f8c01fb6
abisheakp197 <abisheakp197@gmail.com>
2026-04-09 19:42:34 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
 print("File Engine Started")
+
+from scanner import FileScanner
+
+def run():
+    path = "/storage/emulated/0"  # Android storage
+
+    scanner = FileScanner(path)
+    files = scanner.scan()
+
+    print(f"Total files scanned: {len(files)}")
+
+    # Preview first few files
+    for f in files[:5]:
+        print(f)
+
+if __name__ == "__main__":
+    run()
